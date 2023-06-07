0ce9440fb82e66ec135a6dc986d5e9932648f354
kumarmudur <30613897+kumarmudur@users.noreply.github.com>
2023-06-07 22:55:33 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to React Native</Text>
+      <Text>Welcome to React Native Mobile App</Text>
     </View>
   );
 };
